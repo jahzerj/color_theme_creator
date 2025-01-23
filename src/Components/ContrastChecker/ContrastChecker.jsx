2ad742514c25ee9847ad78b9d91ebbf5435e16f7
Jahaziel <jahzerahj@gmail.com>
2025-01-23 17:12:44 +0100

    adding contrast checker component

---
+++ src/Components/ContrastChecker/ContrastChecker.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export default function ContrastChecker({ hex, contrastText }) {
+  const [contrastResult, setContrastResult] = useState();
+
+  useEffect(() => {
+    async function startFetching() {
+        const response = await fetch(
+            `https://www.aremycolorsaccessible.com/api/are-they`
+        {method: "POST",
+        body:JSON.stringify({hex,contrastText}),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    }
+);
+    }
+
+      
